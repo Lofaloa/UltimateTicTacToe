39158902e7b24dbe57c160dc758066f09babb3e7
Farci  Logan 47923 <g47923@PDG.ESI.HEB.SITE>
2019-02-28 14:13:13 +0100

    initial commit

---
+++ MyTicTacToe/src/atlg4/composant/g47923/MyTicTacToe.java
@@ -0,0 +1,20 @@
+package atlg4.composant.g47923;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyTicTacToe extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
